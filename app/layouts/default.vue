684a608a9557666af5db420d84141f4814657afe
Timluk1 <TALukmanov@kpfu.ru>
2025-07-18 15:37:16 +0300

    Add prev components

---
+++ app/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <NuxtLink to="/">
+                    Home
+                </NuxtLink>
+
+                <NuxtLink to="/login">
+                    Login
+                </NuxtLink>
+            </ul>
+        </nav>
+    </header>
+
+    <slot></slot>
+
+    <footer>Footer</footer>
+</template>
